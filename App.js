f1c7f5f47a69367bc1a6adf33585c59eb30aa3a1
Bohdan12367 <lukasevicboda@gmail.com>
2022-02-23 14:10:25 +0200

    1commit1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js bodia Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi delectus dolorum id ipsa. A consequatur distinctio dolores eos error excepturi in, laborum minus natus repellat repellendus sapiente vero voluptatem?  to start working on your app!</Text>
+      <Text>Open up App.js bodialorem1   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi delectus dolorum id ipsa. A consequatur distinctio dolores eos error excepturi in, laborum minus natus repellat repellendus sapiente vero voluptatem?  to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
