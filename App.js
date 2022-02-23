1843eb066016f40f212dbab0e265dcfabaf892d6
Bodian <lykasevicboda@gmail.com>
2022-02-23 14:38:38 +0200

    final try

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>t repellendus sapiente vero voluptatem?  to start working on your app!</Text>
+      <Text>Final Try</Text>
       <StatusBar style="auto" />
     </View>
   );
