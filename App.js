139ff0d656692341f347f0849e7228fab3d4340f
Taras <taras06031@gmail.com>
2022-03-15 19:39:27 +0200

    Some updates with Login Screen

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return ( <
-        View style = { styles.container } >
+    return ( < View style = { styles.container } >
         <
         Text > Open up Appart working on your app! < /Text> <
         StatusBar style = "auto" / >
