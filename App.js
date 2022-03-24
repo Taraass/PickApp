9237f4f158ad69037ee2a3a2844e5978bbfceb78
Taras Tsymbaliak <taras06031@gmail.com>
2022-03-24 19:33:35 +0200

    Added Edit Screen & Update some SearchScreen

--- App.js
+++ App.js
@@ -11,38 +11,40 @@ import LoginScreen from "./Screens/LoginScreen";
 import RegisterScreen from "./Screens/RegisterScreen";
 import PostScreen from "./Screens/PostScreen";
 import SearchScreen from "./Screens/SearchScreen";
+import EditScreen from "./Screens/EditScreen"
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+
 function AppContainer() {
     return (
-    <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen
-            options={{
-                headerShown: false,
-                tabBarIcon: ({tintColor}) => <Ionicons name='ios-home' size={24} color={tintColor} />
-        }} name="Home" component={HomeScreen}/>
-        <Tab.Screen
-            options={{
-                headerShown: false,
-                tabBarIcon: ({tintColor}) => <Ionicons name="ios-add-circle" size={36} color={tintColor}/>
-        }} name="Post" component={PostScreen}/>
-        <Tab.Screen
-            options={{
-                headerShown: false,
-                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-person" size={24} color={tintColor} />
-        }} name="Profile" component={ProfileScreen}/>
-    </Tab.Navigator>
-    )
+        <Tab.Navigator initialRouteName="Home">
+            <Tab.Screen
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({tintColor}) => <Ionicons name='ios-home' size={24} color={tintColor} />
+            }} name="Home" component={HomeScreen}/>
+            <Tab.Screen
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({tintColor}) => <Ionicons name="ios-add-circle" size={36} color={tintColor}/>
+            }} name="Post" component={PostScreen}/>
+            <Tab.Screen
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ tintColor }) => <Ionicons name="ios-person" size={24} color={tintColor} />
+            }} name="Profile" component={ProfileScreen}/>
+            
+        </Tab.Navigator>
+        )
 }
-
 function AuthContainer() {
-    return(
+    return (
         <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
-            <Stack.Screen options={{headerShown: false}} name="Register" component={RegisterScreen} />
-        </Stack.Navigator>
+        <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
     )
 }
 
@@ -50,8 +52,9 @@ function SwitchNavigator() {
     return (
     <Stack.Navigator options={{headerShown: false}} initialRouteName="Loading">
         <Stack.Screen options={{headerShown: false}} name="Loading" component={LoadingScreen} />
-        <Stack.Screen options={{headerShown: false}} name="AppContainer"  component={AppContainer} />
+        <Stack.Screen options={{headerShown: false}} name="AppContainer" component={AppContainer} />
         <Stack.Screen options={{headerShown: false}} name="AuthContainer" component={AuthContainer} />
+        <Stack.Screen options={{headerShown: false}} name="Edit" component={EditScreen} />
         <Stack.Screen options={{headerShown: false}} name="Search" component={SearchScreen} />
     </Stack.Navigator>
     )
